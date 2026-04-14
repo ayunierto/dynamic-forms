874e7a8233ce52bf1863ce6dd788f0db3dddd58b
Alcides Turruellas <ayunierto@gmail.com>
2026-04-14 00:12:50 -0500

    feat: localize page titles and descriptions in DynamicForms component

--- app/dashboard/my-forms/page.tsx
+++ app/dashboard/my-forms/page.tsx
@@ -1,23 +1,38 @@
 'use client';
 
-import { PageContainer } from '@/components/dashboard/page-container';
+import {
+  PageContainer,
+  PageContainerDescription,
+  PageContainerHeader,
+  PageContainerLeftContent,
+  PageContainerRightContent,
+  PageContainerTitle,
+} from '@/components/dashboard/page-container';
 import { SiteHeader } from '@/components/dashboard/site-header';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { NextPage } from 'next';
 
 const DynamicForms: NextPage = ({}) => {
   return (
     <>
-      <SiteHeader title="My Forms" />
+      <SiteHeader title="Mis formularios" />
 
-      <PageContainer
-        title="My Forms"
-        description="Create and manage your forms here."
-        actionButtonText="Create New Form"
-        actionButtonOnClick={() => {
-          console.log('Create New Form clicked');
-        }}
-      >
+      <PageContainer>
+        <PageContainerHeader>
+          <PageContainerLeftContent>
+            <PageContainerTitle>Mis formularios</PageContainerTitle>
+            <PageContainerDescription>
+              Aquí puedes ver y gestionar todos tus formularios.
+            </PageContainerDescription>
+          </PageContainerLeftContent>
+
+          <PageContainerRightContent>
+            <Button variant="default" className="cursor-pointer">
+              Crear nuevo formulario
+            </Button>
+          </PageContainerRightContent>
+        </PageContainerHeader>
         <Card>
           <CardContent>Test card</CardContent>
         </Card>
